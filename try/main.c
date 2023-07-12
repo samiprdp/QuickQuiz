5ad5cd143a50243ef6ed1b242b03e0562cfe00f0
SamTime101 <116491957+samTime101@users.noreply.github.com>
2023-07-12 12:51:58 +0000

    ok

--- try/main.c
+++ try/main.c
@@ -9,6 +9,15 @@ for(j=0;j<3;j++){
 scanf("%d",&a[i][j]);
 }
 }
+//un-transposed
+printf("Printing\n");
+for (i=0;i<2;i++)
+{
+for(j=0;j<3;j++){
+printf("%d\t",a[i][j]);
+}
+printf("\n");
+}
 //transposed
 printf("Printing\n");
 for (i=0;i<3;i++)
