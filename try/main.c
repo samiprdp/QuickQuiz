43cee7366522fc5f20cf92b66402867f9d82da30
SamTime101 <116491957+samTime101@users.noreply.github.com>
2023-07-13 03:15:25 +0000

    ok

--- try/main.c
+++ try/main.c
@@ -1,19 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-int number,answer;
-printf("Enter the number:");
-scanf("%d",&number);
-answer = number &1 ;
-if(answer==0){
-printf("Even\n");
-}
-else if(answer==1){
-printf("odd\n");
-}
-else{
-printf("not allowed");
-}
-return 0;
+    int number, answer;
+    printf("Enter the number:");
+
+    if (scanf("%d", &number) != 1)
+    {
+        printf("Not allowed\n");
+        return 0;
+    }
+
+    answer = number & 1;
+
+    if (answer == 0)
+    {
+        printf("Even\n");
+    }
+    else if (answer == 1)
+    {
+        printf("Odd\n");
+    }
+    else
+    {
+        printf("Not allowed\n");
+    }
+    
+    return 0;
 }
