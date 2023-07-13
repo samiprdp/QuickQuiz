6afcfb25fc33399d9aaec2c3b473dcdc521769d7
SamTime101 <116491957+samTime101@users.noreply.github.com>
2023-07-13 02:50:54 +0000

    ok

--- try/main.c
+++ try/main.c
@@ -1,30 +1,19 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main()
 {
-int a[2][3],i,j;
-printf("Enter the matrix elements:\n");
-for(i=0;i<2;i++){
-for(j=0;j<3;j++){
-scanf("%d",&a[i][j]);
+int number,answer;
+printf("Enter the number:");
+scanf("%d",&number);
+answer = number &1 ;
+if(answer==0){
+printf("Even\n");
 }
+else if(answer==1){
+printf("odd\n");
 }
-//un-transposed
-printf("Printing\n");
-for (i=0;i<2;i++)
-{
-for(j=0;j<3;j++){
-printf("%d\t",a[i][j]);
+else{
+printf("not allowed");
 }
-printf("\n");
-}
-//transposed
-printf("Printing\n");
-for (i=0;i<3;i++)
-{
-for(j=0;j<2;j++){
-printf("%d\t",a[j][i]);
-}
-printf("\n");
-}
+return 0;
 }
