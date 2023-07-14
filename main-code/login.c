b037ab3670bade68f4ebd61c8940ba208c4b47fe
SamTime101 <116491957+samTime101@users.noreply.github.com>
2023-07-14 07:10:40 +0000

    ok

--- main-code/login.c
+++ main-code/login.c
@@ -1,17 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <termios.h>
 
+void starting_logic();
 void login_success(char username[])
 {
+    struct termios term, old_term;
+    tcgetattr(STDIN_FILENO, &term);
+    old_term = term;
+    term.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &term);
+
     printf("Login successful!");
     fflush(stdout);
     sleep(2);
-    
     printf("\rWelcome to Institution %s\n", username);
     fflush(stdout);
-    
-    // Disable input for 2 seconds
-    system("stty -icanon");
     sleep(2);
-    system("stty icanon");
+    tcsetattr(STDIN_FILENO, TCSANOW, &old_term); // Restore original terminal settings    
+    starting_logic();    
+    tcsetattr(STDIN_FILENO, TCSANOW, &old_term); // Restore original terminal settings
+
+}
+
+void starting_logic()
+{
+    struct termios term, old_term;
+    tcgetattr(STDIN_FILENO, &term);
+    old_term = term;
+    term.c_lflag &= ~(ICANON);
+    term.c_lflag |= ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &term);
+    
+    char try[100];
+    printf("\nEnter: ");
+    fflush(stdout);
+    tcflush(STDIN_FILENO, TCIFLUSH); // Clear input buffer
+    scanf("%s", try);
+    printf("%s\n", try);
+    fflush(stdout);
+    
 }
