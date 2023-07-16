bb9d6642a391669267f87cedc4848c767bf671d0
samiprdp <136730010+samiprdp@users.noreply.github.com>
2023-07-16 08:32:45 +0000

    login-system

--- main-code/login.c
+++ main-code/login.c
@@ -1,44 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <termios.h>
-#include "./login.h"
-
-void starting_logic();
-
-void login_success(char username[])
+void login()
 {
-    struct termios term, old_term;
-    tcgetattr(STDIN_FILENO, &term);
-    old_term = term;
-    term.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &term);
-
-    printf("Login successful!");
-    fflush(stdout);
-    sleep(2);
-    printf("\rWelcome to Institution %s\n ", username);
-    fflush(stdout);
-    sleep(2);
-    tcsetattr(STDIN_FILENO, TCSANOW, &old_term); // Restore original terminal settings    
-    starting_logic();    
-    tcsetattr(STDIN_FILENO, TCSANOW, &old_term); // Restore original terminal settings
-}
-
-void starting_logic()
-{
-    struct termios term, old_term;
-    tcgetattr(STDIN_FILENO, &term);
-    old_term = term;
-    term.c_lflag &= ~(ICANON);
-    term.c_lflag |= ECHO;
-    tcsetattr(STDIN_FILENO, TCSANOW, &term);
-    
-    char try[100];
-    printf("\nEnter: ");
-    fflush(stdout);
-    tcflush(STDIN_FILENO, TCIFLUSH); // Clear input buffer
-    scanf("%s", try);
-    printf("%s\n", try);
-    fflush(stdout);
-}
+    printf("Login successful!\n");
+}    
--- main-code/main.c
+++ main-code/main.c
@@ -1,122 +1,43 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "./login.h"
-
-#define MAX_ACCOUNTS 100
-
-struct Account {
-    char username[100];
-    char password[100];
-};
-
-void login(struct Account[], int);
+#include "login.h"
+void logic(char[], char[]);
+int program();
 void fail_login();
-int create(struct Account[], int);
-
 int main()
 {
-    int choose;
-    struct Account accounts[MAX_ACCOUNTS];
-    int numAccounts = 0;
-
-    printf("Welcome to Program\n");
-
-    FILE* file = fopen("main.txt", "r"); // Open the file in read mode
-
-    if (file != NULL)
-    {
-        while (fscanf(file, "username: %s password: %s\n", accounts[numAccounts].username, accounts[numAccounts].password) == 2)
-        {
-            numAccounts++;
-        }
-
-        fclose(file); // Close the file
-    }
-    else
-    {
-        printf("Error opening the file.\n");
-        return -1; // Return an error code to indicate failure
-    }
-
-    printf("1. Create account\n2. Login\n3. Exit\n");
-    scanf("%d", &choose);
-
-    switch (choose)
-    {
-        case 1:
-            numAccounts = create(accounts, numAccounts);
-            login(accounts, numAccounts);
-            break;
-        case 2:
-            login(accounts, numAccounts);
-            break;
-        case 3:
-            exit(0);
-            break;
-        default:
-            printf("Invalid option. Exiting program.\n");
-            break;
-    }
-
+    char name[100], pass[100];
+    printf("Create a user-name: ");
+    scanf("%s", name);
+    printf("create a password: ");
+    scanf("%s", pass);
+    logic(pass, name);
     return 0;
 }
-
-int create(struct Account accounts[], int numAccounts)
+void logic(char pass[], char name[])
 {
-    char name[100], pass[100];
-
-    printf("Create a username: ");
-    scanf("%s", name);
-
-    printf("Create a password: ");
-    scanf("%s", pass);
-
-    FILE* file = fopen("main.txt", "a"); // Open the file in append mode
-
-    if (file != NULL)
+    char username[100], password[100];
+    printf("Enter your username: ");
+    scanf("%s", username);
+    printf("Enter your password: ");
+    scanf("%s", password);
+    if (strcmp(name, username) == 0 && strcmp(pass, password) == 0)
     {
-        fprintf(file, "username: %s password: %s\n", name, pass); // Write the username and password to the file
-        fclose(file); // Close the file
-
-        strcpy(accounts[numAccounts].username, name);
-        strcpy(accounts[numAccounts].password, pass);
-        numAccounts++;
+        system("clear");
+        program();
     }
     else
     {
-        printf("Error opening the file.\n");
-        return numAccounts; // Return the existing number of accounts
+        system("clear");
+        fail_login();
     }
-
-    return numAccounts; // Return the updated number of accounts
 }
-
-void login(struct Account accounts[], int numAccounts)
+int program()
 {
-    char username[100], password[100];
-
-    printf("Enter your username: ");
-    scanf("%s", username);
-
-    printf("Enter your password: ");
-    scanf("%s", password);
-
-    int i;
-    for (i = 0; i < numAccounts; i++)
-    {
-        if (strcmp(username, accounts[i].username) == 0 && strcmp(password, accounts[i].password) == 0)
-        {
-            system("clear");
-            login_success(username);
-            return;
-        }
-    }
-
-    system("clear");
-    fail_login();
+    login();
+    return 0;
 }
-
 void fail_login()
 {
     int choose;
@@ -126,14 +47,14 @@ void fail_login()
     scanf("%d", &choose);
     switch (choose)
     {
-        case 1:
-            main();
-            break;
-        case 2:
-            exit(0);
-            break;
-        default:
-            printf("Invalid option. Exiting program.\n");
-            break;
+    case 1:
+        main();
+        break;
+    case 2:
+        exit(0);
+        break;
+    default:
+        printf("Invalid option. Exiting program.\n");
+        break;
     }
 }
